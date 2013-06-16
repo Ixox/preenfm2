8e1da54f5d8a5d1b7aa3baa1efcf22e47484419c
Ixox <xavier.hosxe@gmail.com>
2013-06-16 23:29:22 +0200

    Put Back bank format.

--- preenFM2/src_synth/Storage.cpp
+++ preenFM2/src_synth/Storage.cpp
@@ -149,7 +149,6 @@ void Storage::saveCombo(int comboNumber, const char*comboName) {
 }
 
 void Storage::createPatchBank() {
-    /*
     for (int k=0; k<PFM_PATCH_SIZE; k++) {
         ((uint8_t*)&reachableParam)[k] = ((uint8_t*)presets)[k];
     }
@@ -162,7 +161,6 @@ void Storage::createPatchBank() {
         savePatch(2, patch, &reachableParam);
         savePatch(3, patch, &reachableParam);
     }
-    */
 }
 
 
